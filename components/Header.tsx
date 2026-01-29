da9d6b6946016dac7846857040902387ffa426e0
PhoneTime <phonetime.au@gmail.com>
2026-01-29 21:18:53 +1100

    upddate links

--- components/Header.tsx
+++ components/Header.tsx
@@ -79,7 +79,7 @@ export default function Header() {
               Home
             </Link>
             <a
-              href="https://shop.phonetime.com.au/shop/category/accessories-8"
+              href="https://shop.phonetime.com.au"
               className="block text-gray-700 hover:text-teal-600 transition"
               onClick={() => setMenuOpen(false)}
               target="_blank"
